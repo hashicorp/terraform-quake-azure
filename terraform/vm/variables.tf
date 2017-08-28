ffd5fa3d9722ebd07806cb420af0a684d494eeb0
Nic Jackson <jackson.nic@gmail.com>
2017-08-28 14:17:12 +0100

    Updated image for VM

--- terraform/vm/variables.tf
+++ terraform/vm/variables.tf
@@ -16,7 +16,7 @@ variable "tenant_id" {
 
 variable "image_uri" {
   description = "Resource ID for the virtual machine image"
-  default = "/subscriptions/c0a607b2-6372-4ef3-abdb-dbe52a7b56ba/resourceGroups/nicgeneral/providers/Microsoft.Compute/snapshots/quakeserver-os-disk"
+  default     = "/subscriptions/c0a607b2-6372-4ef3-abdb-dbe52a7b56ba/resourceGroups/nicgeneral/providers/Microsoft.Compute/snapshots/quake-server"
 }
 
 variable "location" {
