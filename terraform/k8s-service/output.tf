163e787f8b61da5f7c11e3f9497de493fea196fb
Nic Jackson <jackson.nic@gmail.com>
2017-08-31 20:59:45 +0200

    Updated output variables to include DNS

--- terraform/k8s-service/output.tf
+++ terraform/k8s-service/output.tf
@@ -1,3 +1,7 @@
 output "quake_client" {
   value = "${kubernetes_service.quake.load_balancer_ingress.0.ip}"
 }
+
+output "vm_public_dns" {
+  value = "${aws_route53_record.a.name}"
+}
--- terraform/vm/output.tf
+++ terraform/vm/output.tf
@@ -1,3 +1,7 @@
 output "vm_public_ip" {
   value = "${azurerm_public_ip.test.ip_address}"
 }
+
+output "vm_public_dns" {
+  value = "${aws_route53_record.a.name}"
+}
